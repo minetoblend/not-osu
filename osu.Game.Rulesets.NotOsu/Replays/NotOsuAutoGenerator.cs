65a7de0dd41b49c1beeea756325ec4dd0764f025
Marvin Schürz <m.schuerz@hautzy.com>
2025-01-20 13:33:14 +0100

    Initial commit

---
+++ osu.Game.Rulesets.NotOsu/Replays/NotOsuAutoGenerator.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets.NotOsu.Objects;
+using osu.Game.Rulesets.Replays;
+
+namespace osu.Game.Rulesets.NotOsu.Replays
+{
+    public class NotOsuAutoGenerator : AutoGenerator<NotOsuReplayFrame>
+    {
+        public new Beatmap<NotOsuHitObject> Beatmap => (Beatmap<NotOsuHitObject>)base.Beatmap;
+
+        public NotOsuAutoGenerator(IBeatmap beatmap)
+            : base(beatmap)
+        {
+        }
+
+        protected override void GenerateFrames()
+        {
+            Frames.Add(new NotOsuReplayFrame());
+
+            foreach (NotOsuHitObject hitObject in Beatmap.HitObjects)
+            {
+                Frames.Add(new NotOsuReplayFrame
+                {
+                    Time = hitObject.StartTime,
+                    Position = hitObject.Position,
+                    // todo: add required inputs and extra frames.
+                });
+            }
+        }
+    }
+}
---
+++ osu.Game.Rulesets.NotOsu/UI/DrawableNotOsuRuleset.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System.Collections.Generic;
+using osu.Framework.Allocation;
+using osu.Framework.Input;
+using osu.Game.Beatmaps;
+using osu.Game.Input.Handlers;
+using osu.Game.Replays;
+using osu.Game.Rulesets.NotOsu.Objects;
+using osu.Game.Rulesets.NotOsu.Objects.Drawables;
+using osu.Game.Rulesets.NotOsu.Replays;
+using osu.Game.Rulesets.Mods;
+using osu.Game.Rulesets.Objects.Drawables;
+using osu.Game.Rulesets.UI;
+
+namespace osu.Game.Rulesets.NotOsu.UI
+{
+    [Cached]
+    public partial class DrawableNotOsuRuleset : DrawableRuleset<NotOsuHitObject>
+    {
+        public DrawableNotOsuRuleset(NotOsuRuleset ruleset, IBeatmap beatmap, IReadOnlyList<Mod> mods = null)
+            : base(ruleset, beatmap, mods)
+        {
+        }
+
+        protected override Playfield CreatePlayfield() => new NotOsuPlayfield();
+
+        protected override ReplayInputHandler CreateReplayInputHandler(Replay replay) => new NotOsuFramedReplayInputHandler(replay);
+
+        public override DrawableHitObject<NotOsuHitObject> CreateDrawableRepresentation(NotOsuHitObject h) => new DrawableNotOsuHitObject(h);
+
+        protected override PassThroughInputManager CreateInputManager() => new NotOsuInputManager(Ruleset?.RulesetInfo);
+    }
+}
